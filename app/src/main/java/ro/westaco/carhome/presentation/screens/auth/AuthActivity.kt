4ffce5f3f765e0437c09ea847e602b88e10e8ad6
prachiparekh711 <prachi.cubezytech@gmail.com>
2022-06-01 11:08:10 +0530

    Commit 01/06/2022

--- app/src/main/java/ro/westaco/carhome/presentation/screens/auth/AuthActivity.kt
+++ app/src/main/java/ro/westaco/carhome/presentation/screens/auth/AuthActivity.kt
@@ -54,7 +54,6 @@ import java.util.concurrent.Executor
 @AndroidEntryPoint
 class AuthActivity : BaseActivity<AuthModel>() {
 
-
     private var firebaseAuth = FirebaseAuth.getInstance()
 
     // Google
